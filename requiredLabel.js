48d4aa517ce88c6d3b01aee291d3af670b72eaf6
Andrew Stoker <astoker@Andrews-iMac.local>
2015-04-21 09:53:16 -0400

    Moved styles to inline element styles (instead of recreating classes over and over). Also moved div outside of label (to keep semantic html).

--- requiredLabel.js
+++ requiredLabel.js
@@ -4,33 +4,12 @@ define(['jquery', 'knockout'],
             init: function (element, valueAccessor) {
                 // Initially create the required element next to the label
                 var valUnwraped = ko.unwrap(valueAccessor()),
-                    requiredColor = ko.unwrap(valUnwraped.color) || 'red',                    
-                    $scopedCSS = $("<style scoped>" +
-                                        ".requiredLabel-icon-container { " +
-                                            "display: inline-block;" +
-                                        "}" +
-                                        ".requiredLabel-icon-state { " +
-                                            "color: " + requiredColor + ";" +
-                                            "margin-left: 5px;" +
-                                            "font-weight: bold;" +
-                                            "height: .8em;" +
-                                            "width: .8em;" +
-                                        "}" +
-                                        ".requiredLabel-icon-state svg{ " +
-                                            "fill: green;" +
-                                            "width: 100%;" +
-                                            "height: 100%;" +
-                                        "}" +
-                                        ".requiredLabel-text { " +
-                                            "display: inline-block;" +
-                                        "}" +
-                                  "</style>");
-                $requiredStateElem = $("<div class='requiredLabel-icon-container'><div class='requiredLabel-icon-state'>&#42;</div></div>"),
-                $wrappedLabelText = $("<span class='requiredLabel-text'>"+$(element).html().trim()+"</span>");
+                    requiredColor = ko.unwrap(valUnwraped.color) || 'red',                   
+                    
+					$requiredStateElem = $("<div data-requiredLabel='iconState' style='display: inline-block; color: "+requiredColor+"; margin-left: 5px; font-weight: bold; height: .8em; width: .8em;'>&#42;</div>").data('requiredLabel', $(element));
+                //$wrappedLabelText = $("<span style='display: inline-block;'>"+$(element).html().trim()+"</span>");
 
-                $(element).html($wrappedLabelText[0])
-                          .prepend($scopedCSS)
-                          .append($requiredStateElem);
+                $(element).after($requiredStateElem);
 
                 $(element).attr("title", "Required");
             },
@@ -41,9 +20,9 @@ define(['jquery', 'knockout'],
                     completedColor = ko.unwrap(valUnwraped.completedColor) || 'green';
 
                 if (!completedState) {
-                    $(element).find(".requiredLabel-icon-state").html("&#42;");
+                    $(element).next("[data-requiredLabel='iconState']").html("&#42;");
                 } else {
-                    $(element).find(".requiredLabel-icon-state").html("<svg viewBox='0 0 500 500'>"+
+                    $(element).next("[data-requiredLabel='iconState']").html("<svg style='fill: green; width: 100%; height: 100%;' viewBox='0 0 500 500'>"+
                         "<path fill='"+completedColor+"' d='M249.5,1C112,1,1,112,1,249.5S112,498,249.5,498S498,387,498,249.5S386.4,1,249.5,1z M385,136.5"+
                         "c-33.2,47.8-113,170.8-146.2,218.6c-5.3,8-12,9.3-19.9,3.3c-34.6-23.3-68.4-47.2-101.7-70.4c-8.6-6-9.3-12-3.3-20.6"+
                         "c4-6,8-11.3,12-17.3c6-8,12-9.3,20.6-3.3c21.9,15.3,43.2,29.9,64.5,45.2c2,1.3,4,2.7,6.6,4.7c2-2.7,3.3-4.7,4.7-6.6"+
