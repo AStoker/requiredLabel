5db51495a5197d0aa88345a938e5fe8ce7058e21
Andrew Stoker <astoker@Andrews-iMac.local>
2015-04-27 15:49:13 -0400

    Added optional params to toggle visibility of label elements.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A knockout binding to use on labels to indicate whether they are required, and w
 _All options can optionally be knockout observables_  
 
 ## Options:
+* `isRequired`: boolean _(optional)_  
+  * Specifies whether or not to display the required icon. Default state is true. If `isRequired` evaluates to false, then the required label will not show the asterisk, if true, it display.    
 * `completed`: boolean _(optional)_  
   * The state in which the the required field is in. If `completed` evaluates to false, then the default asterisk (*) will be displayed. If `completed` evaluates to true, the asterisk will be replaced with the completed icon. 
   * _If not provided, then there will be no completed state, and the asterisk will always be visible._ 
@@ -11,7 +13,9 @@ _All options can optionally be knockout observables_
   * _If not provided, the default color of red will be used._
 * `completedColor`: string _(optional)_  
   * The string value of the color of the completed state icon. `completedColor` will take any value that evaluates to a color recognized by CSS. 
-  * _If not provided, the default color of green will be used._
+  * _If not provided, the default color of green will be used._  
+* `visibilityTransition`: function(elem) _(optional)_  
+  * A custom function that will animate the transition between visibility of the `isRequired` property. This overwrites the default behavior of just changing the CSS `display` property. The `elem` variable passed in the function is the required label (asterisk) element.      
   
 ## Example:
 ###### Javascript
--- requiredLabel.js
+++ requiredLabel.js
@@ -4,10 +4,12 @@ define(['jquery', 'knockout'],
             init: function (element, valueAccessor) {
                 // Initially create the required element next to the label
                 var valUnwraped = ko.unwrap(valueAccessor()),
+                	isRequired = ko.unwrap(valUnwraped.isRequired),
                     requiredColor = ko.unwrap(valUnwraped.color) || 'red',                   
                     
-					$requiredStateElem = $("<span title='Required' data-requiredLabel='iconState' style='display: inline-block; color: "+requiredColor+"; margin-left: 4px; font-weight: bold; height: .8em; width: .8em;'>&#42;</span>").data('requiredLabelOriginator', $(element)),
+					$requiredStateElem = $("<span title='Required' data-requiredLabel='iconState' style='display: none; color: "+requiredColor+"; margin-left: 4px; font-weight: bold; height: .8em; width: .8em;'>&#42;</span>").data('requiredLabelOriginator', $(element)),
 					$wrappedLabelText = $("<span>"+$(element).html().trim()+"</span>");
+
 
 				$(element).html($wrappedLabelText[0])
                           .append($requiredStateElem);
@@ -17,13 +19,27 @@ define(['jquery', 'knockout'],
             update: function (element, valueAccessor) {
                 // Whenever the value subsequently changes (including on first render/after init)
                 var valUnwraped = ko.unwrap(valueAccessor()),
+                	isRequired = ko.unwrap(valUnwraped.isRequired),             
                     completedState = ko.unwrap(valUnwraped.completed),
-                    completedColor = ko.unwrap(valUnwraped.completedColor) || 'green';
+                    completedColor = ko.unwrap(valUnwraped.completedColor) || 'green',
+                    $elementState = $(element).find("[data-requiredLabel='iconState']"),
+                    visibilityTransitionFunc = valUnwraped.visibilityTransition;          
 
+				if($.isFunction(visibilityTransitionFunc)){
+					visibilityTransitionFunc($elementState[0]);
+				}  else {
+					if(isRequired == null || isRequired == true){ //If null or true
+		                $elementState.css({"display": "inline-block"});
+	                } else { //if false
+		                $elementState.css({"display": "none"});
+	                }
+				}                        
+                
+                
                 if (!completedState) {
-                    $(element).find("[data-requiredLabel='iconState']").html("&#42;");
+                    $elementState.html("&#42;");
                 } else {
-                    $(element).find("[data-requiredLabel='iconState']").html("<svg style='fill: green; width: 100%; height: 100%;' viewBox='0 0 500 500'>"+
+                    $elementState.html("<svg style='fill: green; width: 100%; height: 100%;' viewBox='0 0 500 500'>"+
                         "<path fill='"+completedColor+"' d='M249.5,1C112,1,1,112,1,249.5S112,498,249.5,498S498,387,498,249.5S386.4,1,249.5,1z M385,136.5"+
                         "c-33.2,47.8-113,170.8-146.2,218.6c-5.3,8-12,9.3-19.9,3.3c-34.6-23.3-68.4-47.2-101.7-70.4c-8.6-6-9.3-12-3.3-20.6"+
                         "c4-6,8-11.3,12-17.3c6-8,12-9.3,20.6-3.3c21.9,15.3,43.2,29.9,64.5,45.2c2,1.3,4,2.7,6.6,4.7c2-2.7,3.3-4.7,4.7-6.6"+
